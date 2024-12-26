8a7f5dce706b6d0ffb47b465fc3c2b492d178ac4
Maleva Olga <formaleva@gmail.com>
2024-12-26 11:12:00 +0300

    feat: add Header component

---
+++ src/layout/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="shadow-md bg-white  ">
+      <header className="container mx-auto py-6 flex justify-between items-center ">
+        <h1 className="font-semibold">Where in the world?</h1>
+        <span>Dark mode</span>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
