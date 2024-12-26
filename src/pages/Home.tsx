a5620ea92a611e49aae542d634ed559b7f790a49
Maleva Olga <formaleva@gmail.com>
2024-12-26 11:25:04 +0300

    refactor: update Home component to show list of countries

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,25 @@
+import Header from "../layout/Header";
+import Countrieslist from "./CountriesList";
+import { useGetCountriesQuery } from "../api/apiSlice";
+
 function Home() {
+  const { data: countries = [], isLoading, isSuccess, isError, error } = useGetCountriesQuery();
+
+  let content: React.ReactNode;
+  if (isLoading) {
+    content = <div>Loading...</div>;
+  } else if (isSuccess) {
+    content = <Countrieslist countries={countries} />;
+  } else if (isError) {
+    content = <div>{error.toString()}</div>;
+  }
   return (
-    <div>
-      <h1>HOME</h1>
-    </div>
+    <>
+      <Header />
+      <div className="container mt-12 mx-auto">
+        <div className="flex flex-wrap gap-6 mt-12">{content}</div>
+      </div>
+    </>
   );
 }
 
