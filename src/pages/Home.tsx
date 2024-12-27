0ca786bbfccd1bcdb285b2de880321c7e22ecb67
Maleva Olga <formaleva@gmail.com>
2024-12-27 14:13:09 +0300

    feat: add filters by name and by region to the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,20 @@
 import Header from "../layout/Header";
 import Countrieslist from "./CountriesList";
 import { useGetCountriesQuery } from "../api/apiSlice";
+import FilterByRegion from "../components/FilterByRegion";
+
+import FilterByName from "../components/FilterByName";
 
 function Home() {
   const { data: countries = [], isLoading, isSuccess, isError, error } = useGetCountriesQuery();
 
   let content: React.ReactNode;
+  let filterByRegion: React.ReactNode;
   if (isLoading) {
     content = <div>Loading...</div>;
   } else if (isSuccess) {
+    const regions = Array.from(new Set(countries.map(({ region }) => region)));
+    filterByRegion = content = <FilterByRegion regions={regions} />;
     content = <Countrieslist countries={countries} />;
   } else if (isError) {
     content = <div>{error.toString()}</div>;
@@ -17,6 +23,10 @@ function Home() {
     <>
       <Header />
       <div className="container mt-12 mx-auto">
+        <div className="flex justify-between items-center">
+          {isSuccess && <FilterByName />}
+          {filterByRegion}
+        </div>
         <div className="flex flex-wrap gap-6 mt-12">{content}</div>
       </div>
     </>
