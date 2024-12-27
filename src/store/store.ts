a6eac4433e9c8a43032fe9b07488fd0dedc520c6
Maleva Olga <formaleva@gmail.com>
2024-12-27 14:08:34 +0300

    eat: add filtersSlice reducer to the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../api/apiSlice";
+import { filtersSlice } from "./filtersSlice";
 
 export const store = configureStore({
   reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    filters: filtersSlice.reducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
 });
