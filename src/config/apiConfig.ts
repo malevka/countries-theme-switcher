0c6bd4cb5d66432fca1ea989f3fe373871f6212c
Maleva Olga <formaleva@gmail.com>
2025-01-06 19:02:44 +0300

    refactor: remove unused GET_COUNTRY_BY_CODE, add cca3 field tor GET_COUNTRIES

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,11 +1,7 @@
 export const API_ENDPOINTS = {
   GET_COUNTRIES: {
     url: import.meta.env.VITE_GET_COUNTRIES,
-    fields: ["name", "flags", "population", "capital", "region"]
-  },
-  GET_COUNTRY_BY_CODE: {
-    url: import.meta.env.VITE_GET_COUNTRY_BY_CODE,
-    fields: ["name"]
+    fields: ["name", "flags", "population", "capital", "region", "cca3"]
   },
   GET_COUNTRY_BY_NAME: {
     url: import.meta.env.VITE_GET_COUNTRY_BY_NAME,
