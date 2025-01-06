2130436970e29772c321054e01f7c10895ed1203
Maleva Olga <formaleva@gmail.com>
2025-01-06 13:05:09 +0300

    style: better style for Header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
-    <div className="shadow-md bg-white  ">
-      <header className="container mx-auto py-6 flex justify-between items-center ">
+    <div className="shadow-md bg-white relative ">
+      <header className="container mx-auto px-7 py-6 flex justify-between items-center ">
         <h1 className="font-semibold">Where in the world?</h1>
         <span>Dark mode</span>
       </header>
