049ec36dff83360ede95f7ed2c1ab28805061ccf
Maleva Olga <formaleva@gmail.com>
2025-01-06 12:51:03 +0300

    feat: add navigation link to country details page

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -3,6 +3,7 @@ import Card from "./Card";
 import { useAppSelector } from "../store/hooks";
 import { selectRegion, selectName } from "../store/filtersSlice";
 import { useMemo } from "react";
+import { Link } from "react-router";
 
 interface IProps {
   countries: CountrySummary[];
@@ -10,15 +11,26 @@ interface IProps {
 function CountriesList({ countries }: IProps) {
   const region = useAppSelector(selectRegion);
   const searchName = useAppSelector(selectName);
-  const sortedCountries = useMemo(() => [...countries].sort((a, b) => a.name.localeCompare(b.name)), [countries]);
+  const sortedCountries = useMemo(
+    () => [...countries].sort((a, b) => a.name.localeCompare(b.name)),
+    [countries]
+  );
   const filteredCountries = useMemo(() => {
-    const filteredByRegion = region ? sortedCountries.filter((country) => country.region === region) : sortedCountries;
-    return searchName ? filteredByRegion.filter(({ name }) => name.toLowerCase().startsWith(searchName.toLowerCase())) : filteredByRegion;
+    const filteredByRegion = region
+      ? sortedCountries.filter((country) => country.region === region)
+      : sortedCountries;
+    return searchName
+      ? filteredByRegion.filter(({ name }) =>
+          name.toLowerCase().startsWith(searchName.toLowerCase())
+        )
+      : filteredByRegion;
   }, [sortedCountries, region, searchName]);
   return (
     <div className="grid grid-cols-1 md:grid-cols-[repeat(2,260px)] lg:grid-cols-[repeat(3,260px)] xl:grid-cols-[repeat(4,260px)] justify-center justify-items-center md:justify-between w-full gap-20">
       {filteredCountries.map((country) => (
-          <Card key={country.name} {...country} />
+        <Link key={country.name} to={`/country/${country.name}`}>
+          <Card {...country} />
+        </Link>
       ))}
     </div>
   );
