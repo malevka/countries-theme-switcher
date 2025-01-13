cbedf01418dda5192273303ba0c7d0c3c183d035
Maleva Olga <formaleva@gmail.com>
2025-01-13 18:25:53 +0300

    feat: focus on input when click on search icon

--- src/components/FilterByName.tsx
+++ src/components/FilterByName.tsx
@@ -1,16 +1,25 @@
 import { Input } from "@mui/base/Input";
 import SearchIcon from "@mui/icons-material/Search";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useRef } from "react";
 import { nameFilterChanged, selectName } from "../store/filtersSlice";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 
 function FilterByName() {
   const dispatch = useAppDispatch();
   const name = useAppSelector(selectName);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handleFilterChanged = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
     dispatch(nameFilterChanged(e.target!.value));
+  };
+
+  const handleIconClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
   };
   return (
     <Input
@@ -21,12 +30,14 @@ function FilterByName() {
         input: {
           className:
             "dark:bg-dark-light dark:placeholder:text-white transition-bg",
+          ref: inputRef,
         },
       }}
       aria-label="Search for a country"
       placeholder="Search for a country..."
       startAdornment={
         <SearchIcon
+          onClick={handleIconClick}
           className="mr-4 text-5xl text-gray dark:text-white sm:mr-12 lg:mr-6 lg:text-lg"
           sx={{
             width: { sm: "2.5rem", lg: "1.5rem" },
