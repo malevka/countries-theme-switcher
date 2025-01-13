ad8448cca6ee4581ddd317193b1fd06abe8fe937
Maleva Olga <formaleva@gmail.com>
2025-01-13 16:59:27 +0300

    feat: logo in Header is clickable now

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router";
 import ThemeToggle from "../components/ThemeToggle";
 
 function Header() {
   return (
     <div className="relative bg-white shadow-md dark:bg-dark-light">
       <header className="flex items-center justify-between px-8 py-5 md:container sm:py-12 md:mx-auto md:px-4 lg:py-3">
-        <h1 className="text-xl font-medium sm:text-3xl lg:text-2xl lg:font-semibold">
-          Where in the world?
-        </h1>
+        <Link to="/">
+          <h1 className="text-xl font-medium sm:text-3xl lg:text-2xl lg:font-semibold">
+            Where in the world?
+          </h1>
+        </Link>
         <ThemeToggle />
       </header>
     </div>
