420217641ce28b574f2fcec9b2dc516d9fdcb883
Maleva Olga <formaleva@gmail.com>
2025-01-13 18:10:50 +0300

    fix: correct back button functionality

--- src/pages/CountryDetail.tsx
+++ src/pages/CountryDetail.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@mui/base/Button";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
-import { Link, useParams } from "react-router";
+import { useParams } from "react-router";
 import { useGetCountryByNameQuery } from "../api/apiSlice";
 import ContentWrapper from "../components/ContentWrapper";
 import CountryInfo from "../components/CountryInfo";
@@ -18,17 +18,19 @@ function CountryDetail() {
     <>
       <PageTitle title={`Country Detail - ${params.name}`} />
       <div className="px-3 py-10 sm:px-7 sm:py-20 lg:px-0">
-        <Link to="/">
-          <Button className="transition-bg mb-16 flex items-center gap-5 rounded bg-white py-3 pl-5 pr-5 text-xl font-light tracking-tight shadow-custom-md dark:bg-dark-light dark:text-white sm:mb-32 sm:pl-10 sm:pr-10 sm:text-3xl md:mb-20 lg:gap-2 lg:py-2 lg:pl-7 lg:pr-9 lg:text-base lg:tracking-wider">
-            <KeyboardBackspaceIcon
-              sx={{
-                width: { sm: "2.5rem", lg: "1.5rem" },
-                height: { sm: "2.5rem", lg: "1.5rem" },
-              }}
-            />
-            Back
-          </Button>
-        </Link>
+        <Button
+          className="transition-bg mb-16 flex items-center gap-5 rounded bg-white py-3 pl-5 pr-5 text-xl font-light tracking-tight shadow-custom-md dark:bg-dark-light dark:text-white sm:mb-32 sm:pl-10 sm:pr-10 sm:text-3xl md:mb-20 lg:gap-2 lg:py-2 lg:pl-7 lg:pr-9 lg:text-base lg:tracking-wider"
+          onClick={() => history.back()}
+        >
+          <KeyboardBackspaceIcon
+            sx={{
+              width: { sm: "2.5rem", lg: "1.5rem" },
+              height: { sm: "2.5rem", lg: "1.5rem" },
+            }}
+          />
+          Back
+        </Button>
+
         <ContentWrapper isError={isError} isLoading={isLoading}>
           <CountryInfo country={country!} />
         </ContentWrapper>
